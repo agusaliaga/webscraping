c208e6de127a4fd29aed9c242475b78db4eb46df
Agustina Aliaga <33531021+agusaliaga@users.noreply.github.com>
2019-08-07 15:55:53 -0300

    first commit webscrapper exercise

---
+++ webscrapping.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen as ureq
+from bs4 import BeautifulSoup as soup
+
+my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
+u_client = ureq(my_url)
+page_html = u_client.read()
+u_client.close()
+
+page_soup = soup(page_html, "html.parser")
+containers = page_soup.findAll("div", {"class":"item-container"})
+
+filename = "products.csv"
+f = open(filename, "w")
+
+headers = "brand,product_name,shipping\n"
+f.write(headers)
+
+for container in containers:
+	brand = container.a.img["title"]
+	
+	title_container = container.findAll("a",{"class":"item-title"})
+	product_name = title_container[0].text
+	
+	shipping_container = container.findAll("li", {"class":"price-ship"})
+	shipping = shipping_container[0].text.strip()
+
+	f.write(brand.replace(",", "|") + "," + product_name.replace(",", "|") + "," + shipping + "\n")
+
+f.close()
